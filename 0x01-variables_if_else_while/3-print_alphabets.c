eb1488324d17554186065daf8ea50cfda9b8e121
portable <emmanueltemitopedorcas20@gmail.com>
2021-06-18 15:19:24 +0100

    3-4-alphebet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main(){
+    int ch;
+    /* print uppercase letters */
+
+    for (ch= ‘A’; ch<= ‘Z’; ch++)
+
+    putchar(ch);
+
+    putchar (‘\n’ ) ;
+
+    /* print lowercase letters */
+
+    for (ch= ‘a’; ch<= ‘z’; ch++)
+
+    putchar(ch);
+
+    return (0);
+
+}
+
