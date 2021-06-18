23da82469dc900d5c85dcb80eb5e16006b2a1fb9
portable <emmanueltemitopedorcas20@gmail.com>
2021-06-18 15:33:37 +0100

    3-4-alphebet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-// #include <stdlib.h>
-// #include <time.h>
+
 
 
 void main(){
     int ch;
-    /* print uppercase letters */
     for (ch= ‘A’; ch<= ‘Z’; ch++){
          putchar(ch);
     }
    
-    /* print lowercase letters */
     for (ch= ‘a’; ch<= ‘z’; ch++)
     {
         putchar(ch);
