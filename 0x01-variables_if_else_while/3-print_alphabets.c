e88e27f04de4484b8fb586cc47373a70596b552d
portable <emmanueltemitopedorcas20@gmail.com>
2021-06-18 16:11:59 +0100

    3-4-alphebet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,18 +4,20 @@
 
 
 /* betty style doc for function main goes there */
-int main()
+int main(void)
 {
     int ch;
 
-    for (ch = 'A'; ch <= 'Z'; ch++){
-         putchar(ch);
-    }
-    for (ch = 'a'; ch <= 'z'; ch++){
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
         putchar(ch);
     }
+    for (ch = 'A'; ch <= 'Z'; ch++)
+    {
+         putchar(ch);
+    }
     putchar ('\n');
-    
+
     return(0);
 }
 
