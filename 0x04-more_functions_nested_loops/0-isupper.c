739436d31967c5c7438faf8b2ba8decaa1791251
portable <emmanueltemitopedorcas20@gmail.com>
2021-06-24 16:55:15 +0100

    uppercase and digit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,13 +8,9 @@
  * Return: 1 if c is lowercase, Otherwise 0
  */
 
-int main(void)
+int _isupper(int c)
 {
-    char c;
-    if( isupper(c >= 'A' && c <= 'Z') ) {
-        return (1);
-    }
-    else {
-        return (0);
-    }
+    if(c >= 'A' && c <= 'Z' )
+    return (1);
+    return (0);
 }
