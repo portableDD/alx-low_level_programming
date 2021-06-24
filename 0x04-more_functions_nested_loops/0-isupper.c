30d0d4d1643129bc80c889ba278acfcc69050c44
portable <emmanueltemitopedorcas20@gmail.com>
2021-06-24 14:47:26 +0100

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _islower - Checks if character is lowercase
+ *
+ * @c: Character in ASCII
+ *
+ * Return: 1 if c is lowercase, Otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
